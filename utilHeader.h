e1d01f2d4744970ee9a5df29ccd6023fda8c7f63
hyunwoo0318 <hyunwoo0318@naver.com>
2022-11-12 17:46:00 +0900

    Hash함수 구현완료

--- utilHeader.h
+++ utilHeader.h
@@ -1,10 +1,13 @@
 #pragma once
 #include<iostream>
+#include<cstdio>
 #include<openssl/ec.h>
 #include<openssl/ecdsa.h>
 #include<openssl/obj_mac.h>
 #include<openssl/bn.h>
 #include<openssl/sha.h>
+#include<bitset>
+#include<functional>
 #include<ctime>
 #include<vector>
 #include<stdlib.h>
@@ -13,18 +16,13 @@
 #include<list>
 #include<typeinfo>
 
-
 using namespace std;
 
-/*SHA256을 이용한 HASH*/
 template<typename T>
-unsigned char* hashT(T* t)
+string hashTX(T t)
 {
-	SHA256_CTX c;
-	unsigned char hash[SHA256_DIGEST_LENGTH];
-	unsigned char* ret = hash;
-	SHA256_Init(&c);
-	SHA256_Update(&c, t, sizeof(*t));
-	SHA256_Final(ret, &c);
-	return ret;
+	unsigned char* temp=nullptr;
+	unsigned char* ret = SHA256((unsigned char*)t, sizeof(t), temp);
+	string s(reinterpret_cast<char*>(ret));
+	return s;
 }
