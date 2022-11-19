e53119e42e3f4a73f8981791ca758c9046d3b9cf
hyunwoo0318 <hyunwoo0318@naver.com>
2022-11-19 16:27:25 +0900

    헤더파일과의 병합

--- FullNode.cpp
+++ FullNode.cpp
@@ -1,37 +1,53 @@
-#include"FullNode.h"
+#include"data.h"
 
-//Block FullNode::mining()
-//{
-//    
-//}
-
-bool FullNode::validateTX(Transaction tx)
+class FullNode
 {
-   //1) 판매하려는 물품의 최종 소유자가 tx의 input과 같은가?
-    EC_KEY* sender = tx.getInput();
-    int identifier = tx.getProduct().getIdentifier();
-    if (this->blockChain.findProductOwner(identifier) != sender)
-        return false;
+private:
+    int nodeNum;
+    BlockChain blockChain;
+    vector<Transaction> txPool;
+public:
+    FullNode(int nodeNum)
+    {
+        this->nodeNum = nodeNum;
+    }
+    //Block mining();
+    bool validateTX(Transaction tx)
+    {
+        {
+            //1) 판매하려는 물품의 최종 소유자가 tx의 input과 같은가?
+            EC_KEY* sender = tx.getInput();
+            int identifier = tx.getProduct().getIdentifier();
+            if (this->blockChain.findProductOwner(identifier) != sender)
+                return false;
 
-   //2) immutable 필드값에 변화가 없는가?
-    Product txProduct = tx.getProduct();
-    Product product = this->blockChain.findProduct(identifier);
-    if (!(product == txProduct))
-        return false;
+            //2) immutable 필드값에 변화가 없는가?
+            Product txProduct = tx.getProduct();
+            Product product = this->blockChain.findProduct(identifier);
+            if (!(product == txProduct))
+                return false;
 
-   //3)서명 검증
-    EC_KEY* pubKey = tx.getInput();
-    string digest = tx.getHashTx();
-    const char* c = digest.c_str();
-    const ECDSA_SIG* sig = tx.getSig();
-    int ret = ECDSA_do_verify((unsigned char*)c, 32, sig, pubKey);
-    if (ret != 1)
-        return false;
+            //3)서명 검증
+            EC_KEY* pubKey = tx.getInput();
+            string digest = tx.getHashTx();
+            const char* c = digest.c_str();
+            const ECDSA_SIG* sig = tx.getSig();
+            int ret = ECDSA_do_verify((unsigned char*)c, 32, sig, pubKey);
+            if (ret != 1)
+                return false;
 
-    //valid한 TX로 판명
-    //TODO :  다른 fullNode들에게 넘기기
+            //valid한 TX로 판명
+            //TODO :  다른 fullNode들에게 넘기기
 
-    //자신의 txPool에 tx추가.
-    txPool.push_back(tx);
-    return true;
-}
+            //자신의 txPool에 tx추가.
+            txPool.push_back(tx);
+            return true;
+        }
+    }
+};
+
+//TODO List
+//1. full node는 계속 block과 TX를 받음.
+//2. valid한 tx와 채굴 성공한 block은 넘겨줌.
+//3. userNode로는 아무것도 보내지않음
+//4. FullNode, userNode끼리 주어진 topology대로 연결.
--- UserNode.cpp
+++ UserNode.cpp
@@ -1,112 +1,85 @@
-#include"UserNode.h"
+#include"data.h"
 #include<random>
 
-
-//identifier, publicKey, privateKey를 배정
-UserNode::UserNode(int nodeID)
+class UserNode
 {
-	EC_GROUP* group;
-	random_device rd;
-	mt19937 gen(rd());
-	this->identifier = gen();
+private:
+	EC_KEY* ecKey;
+	int identifier;
+	int nodeID;
+public:
+	UserNode(int nodeID)
+	{
+		EC_GROUP* group;
+		random_device rd;
+		mt19937 gen(rd());
+		this->identifier = gen();
 
-	EC_KEY* secpKey = nullptr;
-	secpKey = EC_KEY_new_by_curve_name(NID_secp256k1);
-	EC_KEY_generate_key(secpKey);
+		EC_KEY* secpKey = nullptr;
+		secpKey = EC_KEY_new_by_curve_name(NID_secp256k1);
+		EC_KEY_generate_key(secpKey);
 
-	BIGNUM const* prv = EC_KEY_get0_private_key(secpKey);
-	EC_POINT const* pub = EC_KEY_get0_public_key(secpKey);
-		
-	this->ecKey = secpKey;
-	this->nodeID = nodeID;
-}
+		BIGNUM const* prv = EC_KEY_get0_private_key(secpKey);
+		EC_POINT const* pub = EC_KEY_get0_public_key(secpKey);
 
-////ECDSA를 이용해서 pubKey, privateKey를 생성
-//pair<char*, char*> UserNode::GenerateKeyPair()
-//{
-//	EC_KEY* secpKey = nullptr;
-//	const BIGNUM* privateKey = nullptr;
-//	const EC_POINT* publicKey = nullptr;
-//
-//	char* privateKeyString;
-//	char* publicKeyString;
-//
-//	//secp256k1으로 부터 key를 만듬
-//	secpKey = EC_KEY_new_by_curve_name(NID_secp256k1);
-//
-//	//위에서 만든 키를 이용해서 privateKey를 생성
-//	EC_KEY_set_private_key(secpKey, privateKey);
-//	auto val = EC_KEY_get0_private_key(secpKey);
-//	privateKeyString = BN_bn2hex(privateKey); // 매우 큰 수를 string으로 변환
-//
-//	//동일한 키를 이용해서 publicKey를 생성
-//	EC_GROUP* group;
-//	group = EC_GROUP_new_by_curve_name(NID_secp256k1);
-//	EC_KEY_set_public_key(secpKey, publicKey);
-//	publicKey = EC_KEY_get0_public_key(secpKey);
-//	publicKeyString = EC_POINT_point2hex(group, publicKey, POINT_CONVERSION_COMPRESSED, NULL);
-//	EC_GROUP_free(group);
-//
-//	return { publicKeyString, privateKeyString };
-//
-//}
+		this->ecKey = secpKey;
+		this->nodeID = nodeID;
+	}
 
-//최초의 물건을 생성하는 TX생성
-Transaction UserNode::createProductTX(string modelNo, int price,string others)
-{
-	
-	Product* product = new Product(time(NULL), modelNo, this->identifier);
-	Transaction* tx = new Transaction(NULL, this->ecKey, price, others, product);
+	Transaction createProductTX(string modelNo, int price, string others)
+	{
 
-	//trID를 구하는 과정
-	string hashRes = hashTX(tx);	
-	tx->setTrID(hashRes);
+		Product* product = new Product(time(NULL), modelNo, this->identifier);
+		Transaction* tx = new Transaction(NULL, this->ecKey, price, others, product);
 
-	//trID를 포함한 전체 tx를 hash해서 전체TX의 hash값을 구한다.
-	hashRes = hashTX(tx);	
-	tx->setHashTx(hashRes);
+		//trID를 구하는 과정
+		string hashRes = hashTX(tx);
+		tx->setTrID(hashRes);
 
-	//privateKey를 이용해서 서명한다.
-	ECDSA_SIG* sig = signTX(hashRes);
-	tx->setSig(sig);
+		//trID를 포함한 전체 tx를 hash해서 전체TX의 hash값을 구한다.
+		hashRes = hashTX(tx);
+		tx->setHashTx(hashRes);
 
-	return *tx;
-}
+		//privateKey를 이용해서 서명한다.
+		ECDSA_SIG* sig = signTX(hashRes);
+		tx->setSig(sig);
 
-//물건을 판매하는 TX 생성
-Transaction UserNode::sellProductTX(Product* product, EC_KEY* dest, string others, int price)
-{	
-	string hashRes;
-	Transaction* tx = new Transaction(this->ecKey,dest, price, others, product);	
-
-	//trID를 구하는 과정
-	string hashRes = hashTX(tx);
-	tx->setTrID(hashRes);
-
-	//trID를 포함한 전체 tx를 hash해서 전체TX의 hash값을 구한다.
-	hashRes = hashTX(tx);
-	tx->setHashTx(hashRes);
-
-	//privateKey를 이용해서 서명한다.
-	ECDSA_SIG* sig = signTX(hashRes);
-	tx->setSig(sig);
-
-	return *tx;
-}
+		return *tx;
+	}
 
 
-//전체 tx를 hash한 값을 이용해 서명한다.
-ECDSA_SIG* UserNode::signTX(string hashRes)
-{
-	const char* c = hashRes.c_str();
-	ECDSA_SIG* sig;	
-	EC_KEY* key = this->ecKey;
-	sig = ECDSA_do_sign((unsigned char*) c, 20, this->ecKey);
-	return sig;
-}
+	Transaction sellProductTX(Product* product, EC_KEY* dest, string others, int price)
+	{
+		string hashRes;
+		Transaction* tx = new Transaction(this->ecKey, dest, price, others, product);
+
+		//trID를 구하는 과정
+		hashRes = hashTX(tx);
+		tx->setTrID(hashRes);
+
+		//trID를 포함한 전체 tx를 hash해서 전체TX의 hash값을 구한다.
+		hashRes = hashTX(tx);
+		tx->setHashTx(hashRes);
+
+		//privateKey를 이용해서 서명한다.
+		ECDSA_SIG* sig = signTX(hashRes);
+		tx->setSig(sig);
+
+		return *tx;
+	}
 
 
+	ECDSA_SIG* signTX(string hashRes)
+	{
+		const char* c = hashRes.c_str();
+		ECDSA_SIG* sig;
+		EC_KEY* key = this->ecKey;
+		sig = ECDSA_do_sign((unsigned char*)c, 20, this->ecKey);
+		return sig;
+	}
+
+	int getIdentifier() { return this->identifier; }
+	EC_KEY* getEcKey() { return this->ecKey; }
 
 
-
-
+};
