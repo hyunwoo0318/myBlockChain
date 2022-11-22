c11f1838278cfee5a46366e98c4524d2d08eb216
hyunwoo0318 <hyunwoo0318@naver.com>
2022-11-22 21:11:42 +0900

    노드 추가

--- UserNode0/UserNode0.cpp
+++ UserNode0/UserNode0.cpp
@@ -1,4 +1,4 @@
-#include"pch.h"
+﻿#include"pch.h"
 #include"UserNode.h"
 
 int main()
@@ -6,5 +6,5 @@ int main()
 	UserNode* userNode0 = new UserNode(0);
 	connectNodes();
 
-	pair<HANDLE, const TCHAR*> a = pipeInfo["F0F1"];
+	pair<HANDLE, LPCSTR> a = pipeInfo["F0F1"];
 }
